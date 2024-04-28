4117774700afe308a211d2d3dccd917b451b4e8a
danielc123 <danielc.123@gmail.com>
2024-04-28 16:08:57 +0200

    Chaging font size to half since the new display is approx half size of the original

--- main.py
+++ main.py
@@ -80,16 +80,16 @@ def pause_button():
 # Main menu function
 
 def main_menu():
-    TITLE_TEXT_SURFACE = bold_font(120).render("Photo Album Player", True, BASE_TEXT_COLOR)
+    TITLE_TEXT_SURFACE = bold_font(60).render("Photo Album Player", True, BASE_TEXT_COLOR)
     TITLE_TEXT_RECT = TITLE_TEXT_SURFACE.get_rect(center=(WIDTH/2, 175))
     SCREEN.blit(TITLE_TEXT_SURFACE, TITLE_TEXT_RECT)
     LOAD_BUTTON = Button(
         surface=MAIN_MENU_BUTTON_BACKGROUND, pos=(WIDTH/2, 415), text_input="Load",
-        font=bold_font(100), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
+        font=bold_font(50), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
     )
     QUIT_BUTTON = Button(
         surface=MAIN_MENU_BUTTON_BACKGROUND, pos=(WIDTH/2, 585), text_input="Quit",
-        font=bold_font(100), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
+        font=bold_font(50), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
     )
     while True:
         current_mouse_pos = pygame.mouse.get_pos()
@@ -130,32 +130,32 @@ def album_player(folder_path):
     
     REWIND_BUTTON = Button(
         surface=REWIND_ICON_SURFACE, pos=(WIDTH/2-100, HEIGHT-150), text_input="",
-        font=bold_font(100), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
+        font=bold_font(50), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
     )
     PAUSE_BUTTON = Button(
         surface=PAUSE_ICON_SURFACE, pos=(WIDTH/2, HEIGHT-150), text_input="",
-        font=bold_font(100), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
+        font=bold_font(50), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
     )
     PLAY_BUTTON = Button(
         surface=PLAY_ICON_SURFACE, pos=(WIDTH/2, HEIGHT-150), text_input="",
-        font=bold_font(100), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
+        font=bold_font(50), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
     )
     SEEK_BUTTON = Button(
         surface=SEEK_ICON_SURFACE, pos=(WIDTH/2+100, HEIGHT-150), text_input="",
-        font=bold_font(100), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
+        font=bold_font(50), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
     )
     LOAD_NEW_ALBUM_BUTTON = Button(
         surface=LOAD_NEW_ALBUM_SURFACE, pos=(WIDTH-325, HEIGHT-150), text_input="",
-        font=bold_font(100), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
+        font=bold_font(50), base_color=BASE_TEXT_COLOR, hovering_color=(111, 255, 233, 1)
     )
 
     previous_time = pygame.time.get_ticks()
     COOLDOWN = 5000
 
-    photo_title_text_surface = bold_font(90).render(image_names[current_image_index], True, BASE_TEXT_COLOR)
+    photo_title_text_surface = bold_font(45).render(image_names[current_image_index], True, BASE_TEXT_COLOR)
     photo_title_text_rect = photo_title_text_surface.get_rect(center=(WIDTH/2, 150))
 
-    image_count_text_surface = regular_font(80).render(f"IMG {current_image_index+1}/{len(image_names)}", True, BASE_TEXT_COLOR)
+    image_count_text_surface = regular_font(40).render(f"IMG {current_image_index+1}/{len(image_names)}", True, BASE_TEXT_COLOR)
     image_count_text_rect = image_count_text_surface.get_rect(center=(300, 755))
 
     new_image_surface = pygame.image.load(image_file_paths[current_image_index])
@@ -221,12 +221,12 @@ def album_player(folder_path):
 
             SCREEN.blit(new_image_surface, new_image_rect)
 
-            photo_title_text_surface = bold_font(90).render(image_names[current_image_index], True, BASE_TEXT_COLOR)
+            photo_title_text_surface = bold_font(45).render(image_names[current_image_index], True, BASE_TEXT_COLOR)
             photo_title_text_rect = photo_title_text_surface.get_rect(center=(WIDTH/2, 150))
 
             SCREEN.blit(photo_title_text_surface, photo_title_text_rect)
             
-            image_count_text_surface = regular_font(80).render(f"IMG {current_image_index+1}/{len(image_names)}", True, BASE_TEXT_COLOR)
+            image_count_text_surface = regular_font(40).render(f"IMG {current_image_index+1}/{len(image_names)}", True, BASE_TEXT_COLOR)
             image_count_text_rect = image_count_text_surface.get_rect(center=(300, 755))
 
             SCREEN.blit(image_count_text_surface, image_count_text_rect)
